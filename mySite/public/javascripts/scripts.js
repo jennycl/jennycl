20e9c7f9b5168e38d9de028de397fcf85df92db9
jennycl <cl2540@nyu.edu>
2017-01-04 01:08:34 -0800

    first commit

---
+++ mySite/public/javascripts/scripts.js
@@ -0,0 +1,33 @@
+/*document.addEventListener('DOMContentLoaded', function(){
+
+  google.load('search', '1');
+
+  google.setOnLoadCallback(OnLoad);
+
+  var search;
+
+  var keyword = 'cat';
+
+  function OnLoad()
+  {
+   search = new google.search.ImageSearch();
+
+   search.setSearchCompleteCallback(this, searchComplete, null);
+
+   search.execute(keyword);
+  }
+
+  function searchComplete()
+  {
+    console.log(";sdlkfja;dsfkl");
+   if (search.results && search.results.length > 0)
+   {
+       var rnd = Math.floor(Math.random() * search.results.length);
+
+       document.body.style.backgroundImage = "url('" + search.results[rnd]['url'] + "')";
+       console.log("url('" + search.results[rnd]['url'] + "')");
+   }
+  }
+
+})
+*/
