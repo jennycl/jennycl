7a2e60782769c7b5876e41919bf4b06fa395f19a
jennycl <cl2540@nyu.edu>
2017-01-12 01:24:36 -0800

    basic pages

--- mySite/routes/index.js
+++ mySite/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jenny' });
 });
 
+router.get('/about', function(req, res, next){
+  res.render('about');
+})
+
+router.get('/projects', function(req, res, next){
+  res.render('projects');
+})
+
+router.get('/skills', function(req, res, next){
+  res.render('skills');
+})
+
+router.get('/contact', function(req, res, next){
+  res.render('contact');
+})
+
+
+router.get('/interests', function(req, res, next){
+  res.render('interests');
+})
+
 module.exports = router;
